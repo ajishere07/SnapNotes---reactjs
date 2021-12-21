f7e7f5687b38ff49fb92e53bd3a89e5d0553af16
Anuj Yadav <anujpy.acct@gmail.com>
2021-12-21 21:14:34 +0530

    added the login endpoint with validations

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -57,7 +57,48 @@ router.post(
       res.json({ jwtAuthToken });
     } catch (err) {
       console.log(err.message);
-      res.status(500).send("some error occured");
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "enter valid email please").isEmail(),
+    body("password", "password cannot be blank ").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: "Hey you try to use correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ errors: "Hey you try to use correct credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const jwtAuthToken = jwt.sign(data, JWT_SECRET);
+      res.json({ jwtAuthToken });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Internal server error");
     }
   }
 );
