6b6826d612b05b998241d6286dbbb33ddcaaaf63
Anuj Yadav <anujpy.acct@gmail.com>
2021-12-21 11:30:20 +0530

    setup the backend and added the auth router

--- Backend/database.js
+++ Backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  "mongodb://localhost:27017/snapNotes?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
+const { body, validationResult } = require("express-validator");
 
 //TODO:in reactApp post request on this /api/auth endpoint
 
-router.get("/", async (req, res) => {
-  res.json({
-    task: "apiTest",
-    message: "success",
-  });
-  const user = await User(req.body);
-  await console.log(req.body);
-  await user.save();
-});
+router.post(
+  "/",
+  [
+    body("name", "enter valid name please").isLength({ min: 3 }),
+    body("email", "enter valid email please").isEmail(),
+    body("password", "password should be 8 character longer").isLength({
+      min: 5,
+    }),
+  ],
+
+  async (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((error) => {
+        console.log(error);
+        res.json({ error: "please enter a unique value" });
+      });
+
+    const user = await User(req.body);
+
+    await user.save();
+  }
+);
 
 module.exports = router;
