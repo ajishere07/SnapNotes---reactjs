21fc71eb8bfadac6ca530841a15b831070b8d767
Anuj Yadav <anujpy.acct@gmail.com>
2021-12-22 20:10:08 +0530

    set up the react router and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./compontents/About";
+import Home from "./compontents/Home";
+import Navbar from "./compontents/Navbar";
+
 function App() {
-  return <div>react app</div>;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
