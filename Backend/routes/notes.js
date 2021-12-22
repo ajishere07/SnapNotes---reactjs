74b6b1426fac215747e669d65db792f0c7f27d4b
Anuj Yadav <anujpy.acct@gmail.com>
2021-12-22 18:45:58 +0530

    endpoints for updating and deleting notes are done✨

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -46,4 +46,77 @@ router.post(
     }
   }
 );
+
+//updating the existing note
+router.put(
+  "/updatenote/:id",
+  fetchuser,
+
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      //new note obj
+      const newUpdatedNote = {};
+      if (title) {
+        newUpdatedNote.title = title;
+      }
+      if (description) {
+        newUpdatedNote.description = description;
+      }
+      if (tag) {
+        newUpdatedNote.tag = tag;
+      }
+
+      //find the note to be updated in the database
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        res.status(404).send("Not Found");
+      }
+
+      if (note.user.toString() !== req.user.id) {
+        return res.status(402).send("Not Allowed");
+      }
+
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: newUpdatedNote,
+        },
+        { new: true }
+      );
+      res.send(note);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+//deleting the existing note
+router.delete(
+  "/deletenote/:id",
+  fetchuser,
+
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      //find the note to be updated in the database
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        res.status(404).send("Not Found");
+      }
+      //allow deletion only if user owns this note
+      if (note.user.toString() !== req.user.id) {
+        return res.status(402).send("Not Allowed");
+      }
+
+      note = await Note.findByIdAndDelete(req.params.id);
+      res.json({ Success: "The note has been deleted" });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 module.exports = router;
